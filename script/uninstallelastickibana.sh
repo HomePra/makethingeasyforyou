a537cbf5da9de46f48d27d2be4985bfdd339da85
carmichaelc09 <127217021+carmichaelc09@users.noreply.github.com>
2023-04-25 10:12:02 -0700

    Update uninstallelastickibana.sh

--- script/uninstallelastickibana.sh
+++ script/uninstallelastickibana.sh
@@ -7,3 +7,5 @@ sudo rm -rf /var/lib/elasticsearch/
 sudo rm -rf /etc/elasticsearch/
 sudo rm -rf /etc/kibana/
 sudo rm -rf /var/lib/kibana/
+sudo rm -rf /opt/agent/
+sudo rm -rf /usr/share/elasticsearch/
